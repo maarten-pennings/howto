122df3cfdb02e15d01e3027ed38870446b7111ca
Maarten Pennings <30388931+maarten-pennings@users.noreply.github.com>
2025-06-01 16:59:18 +0200

    lots of tables

--- c64chars/app.py
+++ c64chars/app.py
@@ -12,7 +12,8 @@ dst_dotsep=1        # pixels between dots in destination image
 dst_charsepx=32     # pixels between chars in destination image in x-direction
 dst_charsepy=3      # pixels between chars in destination image in y-direction
 dst_marginx=32      # margin for labels at left hand side (for row number)
-dst_marginy=96      # margin for labels at the top (for headings and column number)
+dst_marginy0=96     # margin for labels at the top (for headings and column number)
+dst_marginy1=32     # margin for labels at the bottom (for foornote)
 dst_offsetx=16      # offset of the labels in x
 dst_offsety=5       # offset of the labels in y
 dst_font_ascii=20   # font size for ascii
@@ -148,7 +149,7 @@ class Charset:
 
 def demo() :
   charset = Charset("c64fontromhi.pbm","charset uppercase")
-  print( chset.name )
+  print( charset.name )
   pokecode=0x06
   print(charset.char_str(pokecode))
   num=charset.char_num(pokecode)
@@ -156,31 +157,38 @@ def demo() :
   num64= ("0"*64 + bin(num)[2:])[-64:]
   print("0b"+num64)
   for i in range(num_pix) : print( num64[i*8:(i+1)*8] )
-  print( charset.duplicates() )
-  print( charset.wrongrvs() )
-
+  # print( charset.duplicates() )
+  print("duplicates ",end="");
+  for p1,p2 in charset.duplicates() :
+    print( f"({p1:02X},{p2:02X})", end="" )
+  print()
+  #print( charset.wrongrvs() )
+  print("wrongrvs ",end="");
+  for p in charset.wrongrvs() :
+    print( f"{p:02X}", end=" " )
+  print()
 
 def circle(draw,pokecode,color,label) :
   width= dst_charsepy # bice default
   cx = pokecode // num_cx
   cy = pokecode % num_cx
   dst_px = dst_marginx + dst_charsepx + cx*dst_charx 
-  dst_py = dst_marginy + dst_charsepy + cy*dst_chary
+  dst_py = dst_marginy0 + dst_charsepy + cy*dst_chary
   shape = [(dst_px-width,dst_py-width),(dst_px+dst_charbx+width-1,dst_py+dst_charby+width-1)]
   draw.rectangle(shape,outline=color,width=width)
-  draw.text( (dst_px + dst_charbx + 2,dst_py + dst_charby - dst_font_ascii*3), label, color, font=dst_ascii)
+  draw.text( (dst_px + dst_charbx + 3,dst_py + dst_charby - dst_font_ascii*3 + 3), label, color, font=dst_ascii)
 
 
 def table(charset) :
     # setup dest
-    dst_dim= (dst_marginx + dst_charsepx + num_cx*dst_charx , dst_charsepy + num_cy*dst_chary + dst_marginy)
+    dst_dim= (dst_marginx + dst_charsepx + num_cx*dst_charx , dst_marginy0 + dst_charsepy + num_cy*dst_chary + dst_marginy1)
     dst_img = Image.new('RGB', dst_dim, dst_colbg )
     dst_draw = ImageDraw.Draw(dst_img)  
     # draw pixels
     for cx in range(num_cx) :
       for cy in range(num_cy) :
         dst_cx = dst_marginx + dst_charsepx + cx*dst_charx
-        dst_cy = dst_marginy + dst_charsepy + cy*dst_chary
+        dst_cy = dst_marginy0 + dst_charsepy + cy*dst_chary
         for dx in range(num_pix):
           for dy in range(num_pix):
             # create a dot in destination
@@ -203,7 +211,7 @@ def table(charset) :
     # draw col of labels
     for cy in range(num_cy) :
       dst_cx = dst_offsetx
-      dst_cy = 0 + dst_marginy + dst_charsepy + cy*dst_chary + dst_chary/2 - dst_font_labels/2
+      dst_cy = 0 + dst_marginy0 + dst_charsepy + cy*dst_chary + dst_chary/2 - dst_font_labels/2
       dst_draw.text( (dst_cx,dst_cy), f"–{cy%0x100:X}", dst_colfg, font=dst_labels)
     for ascii in range(256) :
       shift=1
@@ -211,80 +219,160 @@ def table(charset) :
       elif  32 <= ascii <= 63: pokecode= ascii
       elif  64 <= ascii <= 95: pokecode= ascii-64
       elif  96 <= ascii <=127: pokecode= ascii-32
-      elif 128 <= ascii <=159: pokecode= ascii; shift=2
+      elif 128 <= ascii <=159: continue # pokecode= ascii; shift=2
       elif 160 <= ascii <=191: pokecode= ascii-64
       elif 192 <= ascii <=254: pokecode= ascii-128; shift=2
-      elif 255 <= ascii <=255: pokecode= ascii-161; shift=2
+      elif 255 <= ascii <=255: pokecode= ascii-161; shift=3
       cx = pokecode // num_cx
       cy = pokecode % num_cx
-      dst_px = dst_marginx + dst_charsepx + cx*dst_charx + dst_charbx + 2
-      dst_py = dst_marginy + dst_charsepy + cy*dst_chary + dst_charby - dst_font_ascii*shift
+      dst_px = dst_marginx + dst_charsepx + cx*dst_charx + dst_charbx + 3
+      dst_py = dst_marginy0 + dst_charsepy + cy*dst_chary + dst_charby - dst_font_ascii*shift + 3
       dst_draw.text( (dst_px,dst_py), f"{ascii:02X}", dst_colfg, font=dst_ascii)
     return dst_img
 
         
+def patchdot(draw,pokecode,col,dx,dy) :
+  cx = pokecode // num_cx
+  cy = pokecode % num_cx
+  # upper left pixel of char cx,cy
+  dst_cx = dst_marginx + dst_charsepx + cx*dst_charx
+  dst_cy = dst_marginy0 + dst_charsepy + cy*dst_chary
+  # upper left pixel of char cx,cy its dot dx,dy
+  dst_dx= dst_cx+dx*dst_dot + dx*dst_dotsep
+  dst_dy= dst_cy+dy*dst_dot + dy*dst_dotsep
+  # form shape
+  d=1
+  shape = [(dst_dx+d,dst_dy+d),(dst_dx+dst_dot-1-d,dst_dy+dst_dot-1-d)]
+  draw.rectangle(shape,fill=col)
+
+
+def patchdots(draw,pokecode,col,dots) :
+  for dx,dy in dots : patchdot(draw,pokecode,col,dx,dy)
+      
+
 def main(charset,basename) :
-    img= table(charset)
-    
-    plain=img.copy()
-    plain_draw = ImageDraw.Draw(plain)  
-    plain_draw.text( (dst_marginx + dst_charsepx,dst_offsety), "Commodore 64 pokecodes [light-blue=on] [ASCII in small]", dst_colfg, font=dst_labels)
-    plain.show()
-    plain.save(basename+'-plain.png')
+  img= table(charset)
+  
+  plain=img.copy()
+  draw = ImageDraw.Draw(plain)  
+  draw.text( (dst_marginx+dst_charsepx,dst_offsety), "Commodore 64 pokecodes", dst_colfg, font=dst_labels)
+  draw.text( (dst_marginx+dst_charsepx,dst_marginy0+dst_charsepy+num_cy*dst_chary+dst_offsety), "Dark blue means pixel active. Small black numbers are the associated ASCII code in hex.", dst_colfg, font=dst_ascii)
+  plain.save(basename+'-1plain.png')
 
-    dups=img.copy()
-    dups_draw= ImageDraw.Draw(dups)  
-    dups_draw.text( (dst_marginx + dst_charsepx,dst_offsety), "Commodore 64 pokecodes - duplicates", dst_colfg, font=dst_labels)
-    for dup in charset.duplicates() :
-      circle(dups_draw,dup[0],"red",f"{dup[1]:02X}")
-      circle(dups_draw,dup[1],"red",f"{dup[0]:02X}")
-    dups.show()
-    dups.save(basename+'-dups.png')
+  dups=img.copy()
+  draw= ImageDraw.Draw(dups)  
+  draw.text( (dst_marginx+dst_charsepx,dst_offsety), "Commodore 64 pokecodes - duplicates", dst_colfg, font=dst_labels)
+  draw.text( (dst_marginx+dst_charsepx,dst_marginy0+dst_charsepy+num_cy*dst_chary+dst_offsety), "Char 60 is likely non-breaking space (20). All other are due to C64 \"doubling\" of pixels (wrt VIC-20), here dotted.", dst_colfg, font=dst_ascii)
+  dupslist=[ (0x40,0x43),(0x42,0x5D),(0x20,0x60),(0x67,0x6A),(0x65,0x74),(0xC0,0xC3),(0xC2,0xDD),(0xA0,0xE0),(0xE7,0xEA),(0xE5,0xF4) ] 
+  if dupslist!=charset.duplicates() : raise Exception("duplicates mismatch")
+  for dup in dupslist :
+    circle(draw,dup[0],"red",f"{dup[1]:02X}")
+    circle(draw,dup[1],"red",f"{dup[0]:02X}")
+  patchdots(draw,0x40,dst_col1, [(x,3) for x in range(num_pix)] ); patchdots(draw,0x43,dst_col1, [(x,4) for x in range(num_pix)] )
+  patchdots(draw,0x42,dst_col1, [(4,y) for y in range(num_pix)] ); patchdots(draw,0x5D,dst_col1, [(3,y) for y in range(num_pix)] )
+  # (0x20,0x60) also in VIC-20
+  patchdots(draw,0x67,dst_col1, [(6,y) for y in range(num_pix)] );  # 0x6A
+  patchdots(draw,0x65,dst_col1, [(1,y) for y in range(num_pix)] );  # 0x74
+  patchdots(draw,0xC0,dst_col0, [(x,3) for x in range(num_pix)] ); patchdots(draw,0xC3,dst_col0, [(x,4) for x in range(num_pix)] )
+  patchdots(draw,0xC2,dst_col0, [(4,y) for y in range(num_pix)] ); patchdots(draw,0xDD,dst_col0, [(3,y) for y in range(num_pix)] )
+  # (0xA0,0xE0) also in VIC-20
+  patchdots(draw,0xE7,dst_col0, [(6,y) for y in range(num_pix)] );  # 0xEA
+  patchdots(draw,0xE5,dst_col0, [(1,y) for y in range(num_pix)] );  # 0xF4
+  dups.save(basename+'-2dups.png')
 
-    wrongrvs=img.copy()
-    wrongrvs_draw= ImageDraw.Draw(wrongrvs)  
-    wrongrvs_draw.text( (dst_marginx + dst_charsepx,dst_offsety), "Commodore 64 pokecodes - mismatching reverse", dst_colfg, font=dst_labels)
-    for wrong in charset.wrongrvs() :
-      circle(wrongrvs_draw,wrong,"red",f"{wrong+128:02X}")
-      circle(wrongrvs_draw,wrong+128,"red",f"{wrong:02X}")
-    wrongrvs.show()
-    wrongrvs.save(basename+'-wrongrvs.png')
-    
-    blocks=img.copy()
-    blocks_draw= ImageDraw.Draw(blocks)  
-    blocks_draw.text( (dst_marginx + dst_charsepx,dst_offsety), "Commodore 64 pokecodes - block graphics", dst_colfg, font=dst_labels)
-    for ix,block in enumerate([32,126,124,226,123,97,255,236,108,127,225,251,98,252,254,160]) :
-      circle(blocks_draw,block,"red",f"#{ix}")
-    blocks.show()
-    blocks.save(basename+'-blocks.png')
-    
-    verfill=img.copy()
-    verfill_draw= ImageDraw.Draw(verfill)  
-    verfill_draw.text( (dst_marginx + dst_charsepx,dst_offsety), "Commodore 64 pokecodes - vertical fill", dst_colfg, font=dst_labels)
-    for ix,block in enumerate([0x20,0x64,0x6F,0x79,0x62,0xF8,0xF7,0xE3,0xA0]) :
-      circle(verfill_draw,block,"red",f"#{ix}")
-    verfill.show()
-    verfill.save(basename+'-verfill.png')
+  wrongrvs=img.copy()
+  draw= ImageDraw.Draw(wrongrvs)  
+  draw.text( (dst_marginx+dst_charsepx,dst_offsety), "Commodore 64 pokecodes - mismatching reverse", dst_colfg, font=dst_labels)
+  draw.text( (dst_marginx+dst_charsepx,dst_marginy0+dst_charsepy+num_cy*dst_chary+dst_offsety), "Char 00 is the only one where the RVS ON is not a true pixel flip (see dotted pixel).", dst_colfg, font=dst_ascii)
+  wrongrvslist=[ 0 ] 
+  if wrongrvslist!=charset.wrongrvs() : raise Exception("wrongrvs mismatch")
+  for wrong in charset.wrongrvs() :
+    circle(draw,wrong,"red",f"{wrong+128:02X}")
+    circle(draw,wrong+128,"red",f"{wrong:02X}")
+  patchdots(draw,0x80,dst_col0, [(5,5)] ); 
+  wrongrvs.save(basename+'-3wrongrvs.png')
+  
+  blocks=img.copy()
+  draw= ImageDraw.Draw(blocks)  
+  draw.text( (dst_marginx+dst_charsepx,dst_offsety), "Commodore 64 pokecodes - block graphics", dst_colfg, font=dst_labels)
+  draw.text( (dst_marginx+dst_charsepx,dst_marginy0+dst_charsepy+num_cy*dst_chary+dst_offsety), "A set of characters for low resolution (80×50) monochrome graphics.", dst_colfg, font=dst_ascii)
+  for ix,block in enumerate([32,126,124,226,123,97,255,236,108,127,225,251,98,252,254,160]) :
+    circle(draw,block,"red",f"#{ix}")
+  blocks.save(basename+'-4blocks.png')
+  
+  verfill=img.copy()
+  draw= ImageDraw.Draw(verfill)  
+  draw.text( (dst_marginx+dst_charsepx,dst_offsety), "Commodore 64 pokecodes - vertical fill", dst_colfg, font=dst_labels)
+  draw.text( (dst_marginx+dst_charsepx,dst_marginy0+dst_charsepy+num_cy*dst_chary+dst_offsety), "Bars to make vertical pixel-accurate gauge.", dst_colfg, font=dst_ascii)
+  for ix,block in enumerate([0x20,0x64,0x6F,0x79,0x62,0xF8,0xF7,0xE3,0xA0]) :
+    circle(draw,block,"red",f"#{ix}")
+  verfill.save(basename+'-5verfill.png')
 
-    horwave=img.copy()
-    horwave_draw= ImageDraw.Draw(horwave)  
-    horwave_draw.text( (dst_marginx + dst_charsepx,dst_offsety), "Commodore 64 pokecodes - horizontal wave", dst_colfg, font=dst_labels)
-    for ix,block in enumerate([0x20,0x64,0x6F,0x52,0x46,0x43,0x44,0x45,0x77,0x63]) : 
-      circle(horwave_draw,block,"red",f"#{ix}")
-    horwave.show()
-    horwave.save(basename+'-horwave.png')
+  horfill=img.copy()
+  draw= ImageDraw.Draw(horfill)  
+  draw.text( (dst_marginx+dst_charsepx,dst_offsety), "Commodore 64 pokecodes - horizontal fill", dst_colfg, font=dst_labels)
+  draw.text( (dst_marginx+dst_charsepx,dst_marginy0+dst_charsepy+num_cy*dst_chary+dst_offsety), "Bars to make horizontal gauge (with the C64 double pixels causing some jumps, see dotted pixels).", dst_colfg, font=dst_ascii)
+  for ix,block in enumerate([0x20,0x65,0x74,0x75,0x61,0xF6,0xEA,0x67,0xA0]) :
+    circle(draw,block,"red",f"#{ix}")
+  patchdots(draw,0x67,dst_col1, [(6,y) for y in range(num_pix)] );
+  patchdots(draw,0x65,dst_col1, [(1,y) for y in range(num_pix)] );
+  horfill.save(basename+'-6horfill.png')
 
-    horfill=img.copy()
-    horfill_draw= ImageDraw.Draw(horfill)  
-    horfill_draw.text( (dst_marginx + dst_charsepx,dst_offsety), "Commodore 64 pokecodes - horizontal fill (two double col issues)", dst_colfg, font=dst_labels)
-    for ix,block in enumerate([0x20,0x65,0x74,0x75,0x61,0xF6,0xEA,0x67,0xA0]) :
-      if ix==1 or ix==7 :
-        circle(horfill_draw,block,"orange",f"#{ix}")
-      else :
-        circle(horfill_draw,block,"red",f"#{ix}")
-    horfill.show()
-    horfill.save(basename+'-horfill.png')
-    
+  verwave=img.copy()
+  draw= ImageDraw.Draw(verwave)  
+  draw.text( (dst_marginx+dst_charsepx,dst_offsety), "Commodore 64 pokecodes - vertical wave", dst_colfg, font=dst_labels)
+  draw.text( (dst_marginx+dst_charsepx,dst_marginy0+dst_charsepy+num_cy*dst_chary+dst_offsety), "Bars to make vertical wave (with the C64 double pixels missing a start and end, see dotted pixels).", dst_colfg, font=dst_ascii)
+  for ix,block in enumerate([0x65,0x74,0x54,0x47,0x42,0x48,0x59,0x6A,0x67]) :
+    circle(draw,block,"red",f"#{ix}")
+  patchdots(draw,0x67,dst_col1, [(6,y) for y in range(num_pix)] );
+  patchdots(draw,0x65,dst_col1, [(1,y) for y in range(num_pix)] );
+  verwave.save(basename+'-7verwave.png')
+  
+  horwave=img.copy()
+  draw= ImageDraw.Draw(horwave)  
+  draw.text( (dst_marginx+dst_charsepx,dst_offsety), "Commodore 64 pokecodes - horizontal wave", dst_colfg, font=dst_labels)
+  draw.text( (dst_marginx+dst_charsepx,dst_marginy0+dst_charsepy+num_cy*dst_chary+dst_offsety), "Characters for a pixel accurate wave.", dst_colfg, font=dst_ascii)
+  for ix,block in enumerate([0x64,0x6F,0x52,0x46,0x43,0x44,0x45,0x77,0x63]) : 
+    circle(draw,block,"red",f"#{ix}")
+  horwave.save(basename+'-8horwave.png')
+
+  midframe=img.copy()
+  draw= ImageDraw.Draw(midframe)  
+  draw.text( (dst_marginx+dst_charsepx,dst_offsety), "Commodore 64 pokecodes - mid cell frames", dst_colfg, font=dst_labels)
+  draw.text( (dst_marginx+dst_charsepx,dst_marginy0+dst_charsepy+num_cy*dst_chary+dst_offsety), "In red two types of corners, in orange the interconnect.", dst_colfg, font=dst_ascii)
+  for ix,block in enumerate([0x70,0x6E,0x6D,0x7D,0x55,0x49,0x4A,0x4B]) :
+    circle(draw,block,"red",f"")
+  for ix,block in enumerate([0x71,0x72,0x73,0x6B,0x5B,0x40,0x42]) :
+    circle(draw,block,"orange",f"")
+  midframe.save(basename+'-9midframe.png')
+
+  outerframe=img.copy()
+  draw= ImageDraw.Draw(outerframe)  
+  draw.text( (dst_marginx+dst_charsepx,dst_offsety), "Commodore 64 pokecodes - outer frames", dst_colfg, font=dst_labels)
+  for ix,block in enumerate([0x4F,0x50,0x4C,0x7A,0x77,0x6F,0x65,0x67]) :
+    circle(draw,block,"red",f"")
+  outerframe.save(basename+'-10outerframe.png')
+
+  symbols=img.copy()
+  draw= ImageDraw.Draw(symbols)  
+  draw.text( (dst_marginx+dst_charsepx,dst_offsety), "Commodore 64 pokecodes - various symbols", dst_colfg, font=dst_labels)
+  for ix,block in enumerate([0x41,0x53,0x58,0x5A, 0x51,0x57,0x5E,0x1E,0x1F,0x56,0x5B]) :
+    circle(draw,block,"red",f"")
+  symbols.save(basename+'-11symbols.png')
+
+  diagonal=img.copy()
+  draw= ImageDraw.Draw(diagonal)  
+  draw.text( (dst_marginx+dst_charsepx,dst_offsety), "Commodore 64 pokecodes - diagonal characters", dst_colfg, font=dst_labels)
+  for ix,block in enumerate([0x4D,0x4E,0x56,0x5F,0x69]) :
+    circle(draw,block,"red",f"")
+  diagonal.save(basename+'-12diagonal.png')
+
+  dither=img.copy()
+  draw= ImageDraw.Draw(dither)  
+  draw.text( (dst_marginx+dst_charsepx,dst_offsety), "Commodore 64 pokecodes - dithering", dst_colfg, font=dst_labels)
+  for ix,block in enumerate([0x5C,0x66,0x68]) :
+    circle(draw,block,"red",f"")
+  dither.save(basename+'-13dither.png')
 
 if __name__ == "__main__":
   #demo()
--- c64chars/readme.md
+++ c64chars/readme.md
@@ -1,38 +1,190 @@
 # Commodore 64 characters
 
-Font ROM of the C64
+Font ROM of the C64.
 
 
 ## Introduction
 
 The character management in the C64 is a bit complex.
 
-- There are two sets: standard, graphics or upper case and the text or lower case set.
-- In one character set (256 chars), the upper 128 or the reverse video of the lower 128.
+- There are two character sets: the default (standard, graphics or upper case) and alternate (text or lower case) set.
+- In one character set (of 256 characters), the upper 128 are the reverse video of the lower 128 characters (all pixels flipped).
 - There is a difference between ASCII codes (or PETSCII if you want) and screen poke codes.
-- Some glyphs (char bitmaps) are duplicated (e.g. ASCII 0x60 and 0x63).
+- Several glyphs (character bitmaps) are duplicated.
 
 This project was an attempt to clarify for myself how the standard set looks like,
+how characters can work together to form bigger "ACSII art",
 and how the mapping from poke codes to ASCII works out.
 
 
 ## Process
 
-I started with a font ROM [dump](c64fontrom1.pbm).
-Actually there is a [double](c64fontrom2.pbm) table if you want both sets.
+I started with a font ROM [dump](c64fontromhi.pbm).
+This is only the default character set.
+The repo also stores the [lower case text set](c64fontromhilo.pbm) if you want to examine it.
 
-I wrote a [python script](app.py) to convert this to a image, separating the pixels ("dots") of the characters
-so that I could clearly see which ones connect.
+I wrote a [python script](app.py) to convert this to overview tables.
+The script separates the pixels of the characters so that I could clearly see which characters connect.
 When you run the script (open `cmd` shell, execute `setup.bat`, execute `run.bat`) it generates the tables
 
-![table](c64fontrom1.png)
 
-![table](c64fontrom2.png)
+## Standard
+
+The first table that is generated just shows all glyphs in a matrix organized according to their pokecodes.
+
+![Standard table](c64fontromhi-1plain.png)
+
+The ASCII code that produces that glyphs is printed in small font to the right of it.
+But this is a bit tricky and still confuses me. I cannot get ASCII codes 0x80-0x9F to do/print anything.
+
+![ASCII](ascii.png)
 
 
-## Key
+## Duplicates
+
+One of the things that wondered me is why there are duplicate glyphs.
+My python script finds them all.
+There quite some, see the red encircled ones below (with in red the pokecode of the duplicate).
+
+![Duplicate glyphs](c64fontromhi-2dups.png)
+
+I think I now know why this is.
+
+One case is the space (0x20) which is also present at 0x60.
+I see that as a space that looks like a space but that is not treated by the kernel
+as a separator. It was already present in the VIC-20 character set, see
+the diagram below with the C64 (left0 and VIC20 (right) character sets.
+
+![C64 vs VIC20](c64-vs-vic20.png)
+
+This also explains the other duplicates.
+The rumor goes that when Commodore moved from the 22 columns VIC-20 to
+the 40 columns C64, there was color distortion at pixel boundaries.
+To mitigate this Commodore made the characters fat: in horizontal
+direction there are always at least two similar colored pixels.
+This doubling is very clear in the above comparison. 
+
+It also meant that the vertical line character with pokecode 0x42,
+a single line in the VIC20 had to double. But so had 0x5D, and then they look alike.
+
+In the above duplicates overview, the pixels added by Commodore 
+for the doubling are dotted by me in the overview.
+
+Since now all vertical lines are double, Commodore decided to also 
+double all horizontal lines, which resulted in 0x40 and 0x43 to become duplicates.
+
+If you are still not convinced, have a look at the keyboard.
+The G and H can be used with the Commodore shift key to give a thin (1 pixel) or thicker (2 pixel)
+vertical bar. In practice, the G gives glyph 0x65 and H gives character 0x74.
+On VIC-20 these are 1 and 2 pixels wide but on the C64 both are 2 pixels wide.
+
+![cbm-G and cbm-H](cbm-G-H.jpg)
+
+Similarly, the N and M can be used with the Commodore shift key to give a thick (2 pixel) or thin (1 pixel)
+vertical bar. In practice, the N gives glyph 0x6A and M gives character 0x67.
+On VIC-20 these are 2 and 1 pixels wide but on the C64 both are 2 pixels wide.
+
+![cbm-N and cbm-M](cbm-N-M.jpg)
+
+It is harder to see, but similar for shift-star and shift-C
+respectively shift-minus and shift-B
+
+![shift star, C, minus, and B](shift_star_C_min_B.jpg)
+
+
+## Reverse video error
+
+My python script also checks if each glyph is a flip of its reverse video counter part.
+To my big surprise, there was one mismatch: for character 0x00 (@).
+
+![Wrong RVS](c64fontromhi-3wrongrvs.png)
+
+
+## Grouping glyphs
+
+This section partiotions all glyphs in catagories.
+
+
+### Coarse block graphics
+
+When using character mode, it is possible to plot 80×50 pixels using the block glyphs.
+
+![Block graphics](c64fontromhi-4blocks.png)
+
+
+### Vertical fill
+
+Use these glyphs for a pixel accurate vertical fill gauge (battery level).
+
+![Vertical fill](c64fontromhi-5verfill.png)
+
+
+### Horizontal fill
+
+There is also a set of glyphs for a horizontal fill gauge, but, due to the pixel doubling, 
+there are some duplicates.
+
+![Horizontal fill](c64fontromhi-6horfill.png)
+
+
+### Vertical wave
+
+Use these glyphs for a pixel accurate vertical tick gauge (stereo speaker balance).
+
+![Vertical wave](c64fontromhi-7verwave.png)
+
+
+### Horizontal wave
+
+Use these glyphs for a horizontal tick gauge, but, due to the pixel doubling, 
+there are some duplicates.
+
+![Horizontal wave](c64fontromhi-8horwave.png)
+
+
+### Mid frames
+
+Draw frames with these "mid-cell" line glyphs.
+
+![Mid frame](c64fontromhi-9midframe.png)
+
+
+### Outside frames
+
+Draw different style of frames with these glyphs.
+
+![Outer frame](c64fontromhi-10outerframe.png)
+
+
+### Various symbols
+
+There us an assortment of symbols, including card suits.
+
+![Symbols](c64fontromhi-11symbols.png)
+
+
+### Diagnoal
+
+There are some diagonal glyphs.
+
+![Diagonal](c64fontromhi-12diagonal.png)
+
+
+### Dithering
+
+For me the least useful, sort of gray scaling via dithering.
+
+![Dithering](c64fontromhi-13dither.png)
+
+
+
+## Links
 
 This [site](https://sta.c64.org/cbm64petkey.html) maps ASCII codes to key strokes.
+
+The VIC-20 char set is from [kreativekorp](https://www.kreativekorp.com/software/fonts/c64/).
+
+
 
 (end)
 
