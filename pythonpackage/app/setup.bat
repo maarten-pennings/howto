b23a17ee711fa151f72e026a9705a9fb06ee4c1a
Maarten Pennings <30388931+maarten-pennings@users.noreply.github.com>
2026-02-12 16:37:54 +0100

    Update setup.bat

--- pythonpackage/app/setup.bat
+++ pythonpackage/app/setup.bat
@@ -1,10 +1,23 @@
 @ECHO OFF
+REM setup.bat template
 
-SET LOCATION=C:\Users\maarten\AppData\Local\Programs\Python\Python39\
-
-%LOCATION%python.exe -m venv env
-CALL env\Scripts\activate.bat
-env\Scripts\python -m pip install --upgrade pip setuptools wheel
-IF EXIST requirements.txt (
-   pip install -r requirements.txt
+REM Set the PYTHONDIR to path for python.exe (must end in \)
+SET PYTHONDIR=C:\Users\maarten\AppData\Local\Programs\Python\Python39\
+IF NOT EXIST %PYTHONDIR%python.exe (
+  ECHO No python.exe in %PYTHONDIR%
+  ECHO Patch line 5 in %~f0
+  EXIT /b
 )
+
+ECHO Creating virtual Python environment
+%PYTHONDIR%python.exe -m venv env
+CALL env\Scripts\activate.bat
+ECHO Upgrading pip
+python -m pip install -q --upgrade pip setuptools wheel
+ECHO Adding packages
+IF EXIST requirements.txt (
+   pip install -q -r requirements.txt
+)
+
+ECHO.Setup done, now use run
+
